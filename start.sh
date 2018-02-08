ec4b13de332b9d511d4163d2c98fda037e2d528d
Attila Kovacs <akovacs101386@gmail.com>
2018-02-07 21:37:53 -0800

    uWSGI proxying

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+workon tc_venv
 sudo nginx -c `pwd`/nginx/nginx.conf -p "`pwd`"
+uwsgi --ini uwsgi_conf.ini &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+sudo killall uwsgi
 sudo killall nginx
