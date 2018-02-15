eaea576d822dda0760d9914858a0c2ecb121e571
Gurpreet Atwal <git@gatwal.com>
2018-02-15 14:24:51 -0800

    move nginx and scripts to top level

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm static/*
-cp ../frontend/build/* static/
-
-workon tc_venv
-sudo nginx -c `pwd`/nginx/nginx.conf -p "`pwd`"
-uwsgi --ini uwsgi_conf.ini &
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo killall uwsgi
-sudo killall nginx
