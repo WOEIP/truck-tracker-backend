906dd20b9af93f52ac24b4a9f51ecd889f77b2a5
Gurpreet Atwal <git@gatwal.com>
2018-02-15 15:02:27 -0800

    install koa.js and create a simple server

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+
+const Koa = require('koa');
+const serve = require('koa-static');
+const Router = require('koa-router');
+
+const config = require('./config');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.use(serve(path.join(__dirname, 'static')));
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+/**
+ * module.parent is set if this file is being imported by another file
+ * We only want to start listening if this as the first file loaded
+ */
+if (!module.parent) {
+  app.listen(config.get('port'));
+}
+
+module.exports = app;
